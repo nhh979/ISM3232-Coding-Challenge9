4f4cf9aed2d4ba93701556a68737b97b1c138e16
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-29 20:36:07 -0400

    Add hover interactivity to div elements.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function main() {
     // Create a div element
     d3.select('body').append('div')
-    
+
     // Add event listeners
     d3.selectAll('div')
         .on('mouseover', function(){
-            d3.select(this).style('background-color', 'red')
+            d3.select(this).style('background-color', 'firebrick')
         })
         .on('mouseout', function(){
             d3.select(this).style('background-color', 'aqua')
