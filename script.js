dec9a3bcaaf03ab577c0082ea077771ddbb525ee
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-05-29 20:34:44 -0400

    Implement interactive behaviors with D3.js.

---
+++ script.js
@@ -0,0 +1,13 @@
+function main() {
+    // Create a div element
+    d3.select('body').append('div')
+    
+    // Add event listeners
+    d3.selectAll('div')
+        .on('mouseover', function(){
+            d3.select(this).style('background-color', 'red')
+        })
+        .on('mouseout', function(){
+            d3.select(this).style('background-color', 'aqua')
+        })
+}
